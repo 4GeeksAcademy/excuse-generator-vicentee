0ce20ed31130c5b0680574c2773b3fa353591dfe
Vicho <vicentecastroibarra@gmail.com>
2025-12-04 19:59:32 +0000

    excuseterminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying']; 
+
+  const sujeto = who[Math.floor(Math.random() * who.length)];
+  const verbo = action[Math.floor(Math.random() * action.length)];
+  const objeto = what[Math.floor(Math.random() * what.length)];
+  const cuando = when[Math.floor(Math.random() * when.length)];
+
+  const excuse = `${sujeto} ${verbo} ${objeto} ${cuando}.`;
+
+  document.getElementById("excuse").innerHTML = excuse;
+
+  console.log("Excuse generated:", excuse);
 };
